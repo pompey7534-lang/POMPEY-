5408863fb1126fc1e46071c732c394350986b7ab
pompey7534-lang <pompey7534@gmail.com>
2026-07-09 08:07:07 +0530

    firebase-config.js
    Set up Firebase configuration and authentication.

---
+++ firebase-config.js
@@ -0,0 +1,22 @@
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAXvZ3pJReUhXPweOEFe09X6-ImhJwzs",
+  authDomain: "pompey-977ea.firebaseapp.com",
+  projectId: "pompey-977ea",
+  storageBucket: "pompey-977ea.firebasestorage.app",
+  messagingSenderId: "607741100294",
+  appId: "1:607741100294:web:3a221386c2155979681e87"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Authentication
+const auth = getAuth(app);
+
+// Export auth so other files can use it
+export { auth };
